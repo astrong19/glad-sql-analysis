c4cfad118209775751c773679ddfe187223095ec
astrong19 <astrong@wri.org>
2017-02-28 15:06:16 -0500

    iteration 2

--- gladanalysis/routes/api/v1/ms_router.py
+++ gladanalysis/routes/api/v1/ms_router.py
@@ -12,21 +12,18 @@ from gladanalysis.responders import ErrorResponder
 from gladanalysis.utils.http import request_to_microservice
 
 @endpoints.route('/gladanalysis', methods=['GET'])
-def get_geo():
-
-    geostore = request.args.get('geostore')
-    return geostore
-
 def query_glad():
     """Query GLAD"""
     logging.info('QUERYING GLAD')
 
+    geostore = request.args.get('geostore')
+
     url = 'http://staging-api.globalforestwatch.org/query/'
     sql = "?sql=count(julian_day) from index_b846230fcec0420892d13fc11ea7e32b where ((year = '2015' and julian_day >= 1) or (year = '2016') or (year = '2017' and julian_day <= 58))"
     f = '&format=json'
-    geostore = '&geostore=%s' %(get_geo)
+    prefix = '&geostore='
 
-    request = url + geostore + sql + f
+    request = url + prefix + geostore + sql + f
     r = requests.get(request)
 
     return jsonify(r.json()), 200
