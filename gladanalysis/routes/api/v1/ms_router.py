f62525f09b10bf37969a97cefbf4a96209964913
astrong19 <astrong@wri.org>
2017-02-28 16:55:22 -0500

    add date arguments

--- gladanalysis/routes/api/v1/ms_router.py
+++ gladanalysis/routes/api/v1/ms_router.py
@@ -3,6 +3,7 @@ import json
 import csv
 import StringIO
 import logging
+import datetime
 
 from flask import jsonify, request, Response, stream_with_context
 import requests
@@ -11,16 +12,43 @@ from . import endpoints
 from gladanalysis.responders import ErrorResponder
 from gladanalysis.utils.http import request_to_microservice
 
+#dates should be year then julian dates
+#example request: localhost:9000/gladanalysis?geostore=939a166f7e824f62eb967f7cfb3462ee&period=2016-1,2017-1-1&confidence=3
+
 @endpoints.route('/gladanalysis', methods=['GET'])
 def query_glad():
     """Query GLAD"""
     logging.info('QUERYING GLAD')
 
-    geostore = request.args.get('geostore')
+    geostore = request.args.get('geostore', None)
+    period = request.args.get('period', None)
+    confidence = request.args.get('confidence', None)
 
-    datasetID = '274b4818-be18-4890-9d10-eae56d2a82e5'
+    if not geostore or not period:
+        return jsonify({'errors': [{
+            'status': '400',
+            'title': 'geostore and period should be set'
+            }]
+        }), 400
+
+    if len(period.split(',')) < 2:
+        return jsonify({'errors': [{
+            'status': '400',
+            'title': 'Period needs 2 arguments'
+            }]
+        }), 400
+
+    period_from = period.split(',')[0]
+    period_to = period.split(',')[1]
+
+    from_year = period_from.split("-")[0]
+    from_date = period_from.split("-")[1]
+    to_year = period_to.split("-")[0]
+    to_date = period_to.split("-")[1]
+
     url = 'http://staging-api.globalforestwatch.org/query/'
-    sql = "?sql=select count(julian_day) from index_b846230fcec0420892d13fc11ea7e32b where ((year = '2015' and julian_day >= 1) or (year = '2016') or (year = '2017' and julian_day <= 58))"
+    datasetID = '274b4818-be18-4890-9d10-eae56d2a82e5'
+    sql = "?sql=select count(julian_day) from index_b846230fcec0420892d13fc11ea7e32b where ((year = %s and julian_day >= %s) or (year = '2016') or (year = %s and julian_day <= %s))" %(from_year, from_date, to_year, to_date)
     prefix = '&geostore='
     f = '&format=json'
 
